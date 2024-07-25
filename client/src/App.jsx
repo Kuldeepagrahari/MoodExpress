a120025ebad15dfb6281fd920ab07f4afcd8cb5c
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2024-07-25 14:36:59 +0530

    sam i'm going to create an application where people can express their mood by posting their images with some message

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+
+
+import { RiImageAddFill } from "react-icons/ri";
+
+function App() {
+  
+
+  return (
+    <div className="app">
+      <header>
+        <h1>MoodExpress.</h1>
+        <button style={{backgroundColor:"transparent", fontSize:"3vw"}}><RiImageAddFill/>
+        <input type="file" />
+        </button>
+      </header>
+
+      <div className="container">
+         
+      </div>
+     
+    </div>
+  )
+}
+
+export default App
